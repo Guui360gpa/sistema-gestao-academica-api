898a824f2327a5fa5febdfe59977361f6de1bce8
Guui360gpa <guipaiva.alves14@gmail.com>
2026-06-06 00:00:44 -0300

    Gerando projeto com Spring

---
+++ sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/SistemagestaoacademicaApplication.java
@@ -0,0 +1,18 @@
+package br.com.sistemagestaoacademica;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemagestaoacademicaApplication implements CommandLineRunner {
+
+	public static void main(String[] args){
+		SpringApplication.run(SistemagestaoacademicaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
---
+++ sistemagestaoacademica/src/test/java/br/com/sistemagestaoacademica/SistemagestaoacademicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistemagestaoacademica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemagestaoacademicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
