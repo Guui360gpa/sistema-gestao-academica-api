e8ba102653855aea7acc75aba01aaaee0a97ff4c
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-15 13:21:47 -0300

    refactor: service CadastrarCurso

---
+++ sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/exception/CursoJaCadastrado.java
@@ -0,0 +1,7 @@
+package br.com.sistemagestaoacademica.exception;
+
+public class CursoJaCadastrado extends RuntimeException {
+  public CursoJaCadastrado(String message) {
+    super(message);
+  }
+}
---
+++ sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/exception/MatriculaDuplicadaException.java
@@ -0,0 +1,7 @@
+package br.com.sistemagestaoacademica.exception;
+
+public class MatriculaDuplicadaException extends RuntimeException {
+  public MatriculaDuplicadaException(String message) {
+    super(message);
+  }
+}
