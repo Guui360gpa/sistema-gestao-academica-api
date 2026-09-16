165092cdf16b50d4a465a929ab976c5d141531d9
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-15 23:20:15 -0300

    refactor: service DesativarTurma

--- sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/service/turma/DesativarTurma.java
+++ sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/service/turma/DesativarTurma.java
@@ -1,50 +1,30 @@
 package br.com.sistemagestaoacademica.service.turma;
 
+import br.com.sistemagestaoacademica.exception.TurmaNaoEncontradaException;
 import br.com.sistemagestaoacademica.models.Status;
 import br.com.sistemagestaoacademica.models.Turma;
-import br.com.sistemagestaoacademica.service.BaseService;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.sistemagestaoacademica.repository.TurmaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@org.springframework.stereotype.Service
-public class DesativarTurma extends BaseService {
+@Service
+@RequiredArgsConstructor
+public class DesativarTurma{
 
-    @Autowired
-    private ListarTurmasAtivas listarTurmasAtivas;
+    private final TurmaRepository turmaRepository;
 
-    public void desativar() {
-        Turma turmaEncontrada = null;
-        List<Turma> turmasEncontradas;
+    public void desativar(Long idTurma) {
+        Turma turma = turmaRepository.findById(idTurma)
+                .orElseThrow(() -> new TurmaNaoEncontradaException("Turma não encontrada"));
 
-        System.out.println("Qual turma você deseja DESATIVAR?");
-        listarTurmasAtivas.listar();
-        while (turmaEncontrada == null){
-            var nomeTurma = read.nextLine();
-            turmasEncontradas = turmaRepository.buscarTurmaAtivaPorNome(nomeTurma, Status.ATIVADA);
-            if (turmasEncontradas.isEmpty()) {
-                System.out.println("\nTurma ativa não encontrada! Tente Novamente.");
-            }else {
-                turmasEncontradas.forEach(t ->
-                        System.out.printf("ID: %s | Turma: %s | Professor: %s | Curso: %s\n",
-                                t.getId(),
-                                t.getNome(),
-                                t.getProfessor().getNome(),
-                                t.getCurso().getNome()));
-                System.out.println("\nDigite o ID da turma que deseja desativar: ");
-                Long idSelecionado = lerLong();
-                turmaEncontrada = turmasEncontradas.stream()
-                        .filter(p -> p.getId().equals(idSelecionado))
-                        .findFirst()
-                        .orElse(null);
+        turma.setStatusTurma(Status.DESATIVADA);
 
-                if (turmaEncontrada == null) {
-                    System.out.println("\nID inválido! Tente novamente:");
-                }
-            }
-        }
+        salvarTurmaNoBanco(turma);
+    }
 
-        turmaEncontrada.setStatusTurma(Status.DESATIVADA);
-        turmaRepository.save(turmaEncontrada);
+    private void salvarTurmaNoBanco(Turma turma){
+        turmaRepository.save(turma);
     }
 }
