e66674eb43eb7a28dab14b5a60da3817b1449bd1
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-16 11:59:30 -0300

    refactor: controller AlunoController

--- sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/SistemagestaoacademicaApplication.java
+++ sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/SistemagestaoacademicaApplication.java
@@ -1,23 +1,23 @@
-package br.com.sistemagestaoacademica;
-
-import br.com.sistemagestaoacademica.principal.MenuPrincipal;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SistemagestaoacademicaApplication implements CommandLineRunner {
-
-	@Autowired
-	private MenuPrincipal menuPrincipal;
-
-	public static void main(String[] args){
-		SpringApplication.run(SistemagestaoacademicaApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		menuPrincipal.menu();
-	}
-}
+//package br.com.sistemagestaoacademica;
+//
+//import br.com.sistemagestaoacademica.principal.MenuPrincipal;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class SistemagestaoacademicaApplication implements CommandLineRunner {
+//
+//	@Autowired
+//	private MenuPrincipal menuPrincipal;
+//
+//	public static void main(String[] args){
+//		SpringApplication.run(SistemagestaoacademicaApplication.class, args);
+//	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		menuPrincipal.menu();
+//	}
+//}
---
+++ sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/controller/AlunoController.java
@@ -0,0 +1,47 @@
+package br.com.sistemagestaoacademica.controller;
+
+
+import br.com.sistemagestaoacademica.dto.AlunoRequestDto;
+import br.com.sistemagestaoacademica.dto.AlunoResponseDto;
+import br.com.sistemagestaoacademica.dto.MatriculaRequestDto;
+import br.com.sistemagestaoacademica.dto.MatriculaResponseDto;
+import br.com.sistemagestaoacademica.service.aluno.CadastrarAluno;
+import br.com.sistemagestaoacademica.service.aluno.ListarAlunoPorTurma;
+import br.com.sistemagestaoacademica.service.aluno.MatricularAluno;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/aluno")
+@RequiredArgsConstructor
+public class AlunoController{
+
+    private final CadastrarAluno cadastrarAluno;
+    private final ListarAlunoPorTurma listarAlunoPorTurma;
+    private final MatricularAluno matricularAluno;
+
+
+    @PostMapping
+    public ResponseEntity<AlunoResponseDto> cadastro(@Valid @RequestBody AlunoRequestDto dto){
+        AlunoResponseDto alunoResponse = cadastrarAluno.cadastrar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(alunoResponse);
+    }
+
+    @GetMapping("/turma/{turmaId}")
+    public ResponseEntity<List<AlunoResponseDto>> listaPorTurma(@PathVariable Long turmaId){
+        List<AlunoResponseDto> alunosResponse = listarAlunoPorTurma.listar(turmaId);
+        return ResponseEntity.ok(alunosResponse);
+    }
+
+    @PostMapping("/matricula")
+    public ResponseEntity<MatriculaResponseDto> matricula(@Valid @RequestBody MatriculaRequestDto dto){
+        MatriculaResponseDto matriculaResponse = matricularAluno.matricular(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(matriculaResponse);
+    }
+}
