f4b7a05ff84738bf1df939f7337af8a86af2e0ad
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-16 12:11:27 -0300

    refactor: controller CursoController

--- sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/controller/AlunoController.java
+++ sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/controller/AlunoController.java
@@ -10,7 +10,6 @@ import br.com.sistemagestaoacademica.service.aluno.ListarAlunoPorTurma;
 import br.com.sistemagestaoacademica.service.aluno.MatricularAluno;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/aluno")
+@RequestMapping("/alunos")
 @RequiredArgsConstructor
 public class AlunoController{
 
---
+++ sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/controller/CursoController.java
@@ -0,0 +1,50 @@
+package br.com.sistemagestaoacademica.controller;
+
+import br.com.sistemagestaoacademica.dto.CursoRequestDto;
+import br.com.sistemagestaoacademica.dto.CursoResponseDto;
+import br.com.sistemagestaoacademica.service.curso.CadastrarCurso;
+import br.com.sistemagestaoacademica.service.curso.DesativarCurso;
+import br.com.sistemagestaoacademica.service.curso.ListarCursosAtivos;
+import br.com.sistemagestaoacademica.service.curso.ListarCursosDesativados;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cursos")
+@RequiredArgsConstructor
+public class CursoController{
+
+    private final CadastrarCurso cadastrarCurso;
+    private final DesativarCurso desativarCurso;
+    private final ListarCursosDesativados listarDesativados;
+    private final ListarCursosAtivos listarAtivos;
+
+    @PostMapping
+    public ResponseEntity<CursoResponseDto> cadastro(@Valid @RequestBody CursoRequestDto dto){
+        CursoResponseDto cursoResponse = cadastrarCurso.cadastrar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cursoResponse);
+    }
+
+    @GetMapping("/desativados")
+    public ResponseEntity<List<CursoResponseDto>> listaDesativos(){
+        List<CursoResponseDto> cursosResponse = listarDesativados.listar();
+        return ResponseEntity.ok(cursosResponse);
+    }
+
+    @GetMapping("/ativados")
+    public ResponseEntity<List<CursoResponseDto>> listaAtivos(){
+        List<CursoResponseDto> cursosResponse = listarAtivos.listar();
+        return ResponseEntity.ok(cursosResponse);
+    }
+
+    @DeleteMapping("/{cursoId}")
+    public ResponseEntity<CursoResponseDto> desativa(@PathVariable Long cursoId){
+        CursoResponseDto cursoResponse = desativarCurso.desativar(cursoId);
+        return ResponseEntity.ok(cursoResponse);
+    }
+}
