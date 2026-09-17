8d8cf0807a2c8ad503ed74fbbdecfe835fbc013e
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-17 13:04:54 -0300

    feat: adiciona GlobalExceptionHandler

---
+++ sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/exception/GlobalExceptionHandler.java
@@ -0,0 +1,66 @@
+package br.com.sistemagestaoacademica.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    //400
+    @ExceptionHandler({
+            DataInvalidaException.class,
+            EmailInvalidoException.class
+    })
+    public ResponseEntity<ErrorResponseDto> handleBadRequest(RuntimeException ex){
+        return construirResposta(HttpStatus.BAD_REQUEST, ex);
+    }
+
+    //404
+    @ExceptionHandler({
+            AlunoNaoEncontradoException.class,
+            CursoNaoEncontradoException.class,
+            ProfessorNaoEncontradoException.class,
+            TurmaNaoEncontradaException.class
+    })
+    public ResponseEntity<ErrorResponseDto> handleNotFound(RuntimeException ex){
+        return construirResposta(HttpStatus.BAD_REQUEST,ex);
+    }
+
+    //409
+    @ExceptionHandler({
+            CursoJaCadastradoException.class,
+            CursoJaDesativadoException.class,
+            EmailJaCadastradoException.class,
+            MatriculaDuplicadaException.class,
+            ProfessorComTurmaAtivaException.class,
+            ProfessorJaDesativadoException.class
+    })
+    public ResponseEntity<ErrorResponseDto> handleConflict(RuntimeException ex){
+        return construirResposta(HttpStatus.CONFLICT,ex);
+    }
+
+    //422
+    @ExceptionHandler({
+            ListaCursosVazioException.class,
+            ListaProfessorVazioException.class,
+            ListaTurmaVaziaException.class,
+            TurmaVaziaException.class
+    })
+    public ResponseEntity<ErrorResponseDto> handleUnprocessable(RuntimeException ex) {
+        return construirResposta(HttpStatus.UNPROCESSABLE_CONTENT,ex);
+    }
+
+    private ResponseEntity<ErrorResponseDto> construirResposta(HttpStatus status, RuntimeException ex) {
+        ErrorResponseDto body = new ErrorResponseDto(
+                LocalDateTime.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                ex.getMessage()
+        );
+        return ResponseEntity.status(status).body(body);
+    }
+}
