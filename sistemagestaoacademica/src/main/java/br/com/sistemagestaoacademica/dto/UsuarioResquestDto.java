533f5d59b760c496f531ce152d41c1be8f8ca88a
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-19 20:33:29 -0300

    feat: adiciona dto UsuarioRequest

---
+++ sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/dto/UsuarioResquestDto.java
@@ -0,0 +1,14 @@
+package br.com.sistemagestaoacademica.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UsuarioResquestDto(
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Email inválido")
+        String email,
+
+        @NotBlank(message = "Senha é obrigatória")
+        String senha
+) {
+}
