7ae14ac7d1814a77bb1ed8bff86671011e13df52
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-19 18:25:23 -0300

    feat: adiciona repository de Usuario

---
+++ sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/repository/UsuarioRepository.java
@@ -0,0 +1,19 @@
+package br.com.sistemagestaoacademica.repository;
+
+import br.com.sistemagestaoacademica.models.Status;
+import br.com.sistemagestaoacademica.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+
+    boolean existsByEmail(String email);
+
+    List<Usuario> findByStatus(@Param("status") Status status);
+
+    Optional<Usuario> findByEmail (String email);
+
+}
