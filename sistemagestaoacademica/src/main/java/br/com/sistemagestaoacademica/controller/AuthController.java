e2b08424115011a4e49b0a540a9ea12fd4def51e
Guui360gpa <guipaiva.alves14@gmail.com>
2026-09-19 21:17:15 -0300

    feat: adiciona controller de Auth

--- sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/controller/AuthController.java
+++ sistemagestaoacademica/src/main/java/br/com/sistemagestaoacademica/controller/AuthController.java
@@ -1,4 +1,25 @@
 package br.com.sistemagestaoacademica.controller;
 
+import br.com.sistemagestaoacademica.dto.LoginRequestDto;
+import br.com.sistemagestaoacademica.dto.LoginResponseDto;
+import br.com.sistemagestaoacademica.service.login.AutenticarUsuario;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AutenticarUsuario autenticarUsuario;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto dto) {
+        LoginResponseDto loginResponse = autenticarUsuario.login(dto);
+        return ResponseEntity.ok(loginResponse);
+    }
+
 }
